b99ba1cc68081852de6955620f6bf4469b75ff24
Luzia30 <luzia.simao08@aluno.ifce.edu.br>
2025-08-12 18:41:11 -0300

    Add files via upload

---
+++ ControleBonificacao.java
@@ -0,0 +1,13 @@
+package Empresa;
+
+public class ControleBonificacao {
+    private double soma;
+
+    public void registra(Funcionario f) {
+        this.soma += f.getBonificacao();
+    }
+
+    public double getSoma() {
+        return soma;
+    }
+}
